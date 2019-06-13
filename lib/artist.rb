dc0fe7a38364598d60bcaedf0f630cd71c9c5a83
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-13 18:22:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,13 +21,14 @@ class Artist
     self.class.all << self
   end
   
-  def self.find_or_create_by_name
-  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
   
-  def self.create_by_name
-    
-  end
+  def self.create_by_name(name)
+    self.new(name).tap(&:save)
   
-  def self.find_by_name
+  def self.find_by_name(name)
+    self.all.detect {|artist| artist.name == name}
+  end
 end
